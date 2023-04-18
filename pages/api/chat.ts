4f20af098c0a470e6fd89777101a440b34f46fb9
esponges <esponges@gmail.com>
2023-04-17 19:39:50 -0600

    try with formidable

--- pages/api/chat.ts
+++ pages/api/chat.ts
@@ -4,12 +4,31 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import { makeChain } from '@/utils/makechain';
 // import { pinecone } from '@/utils/pinecone-client';
 // import { PINECONE_INDEX_NAME, PINECONE_NAME_SPACE } from '@/config/pinecone';
+import formidable from 'formidable';
+
+export const config = {
+  api: {
+    bodyParser: false
+  }
+}
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
-  const { question, history } = req.body;
+  // will receive a FormData object
+  const formData = await new Promise((resolve, reject) => {
+    const form = new formidable.IncomingForm();
+    form.parse(req, (err, fields, files) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve({ fields, files });
+    });
+  });
+
+  const { question, history } = formData.fields;
 
   console.log('question', question);
 
@@ -19,13 +38,14 @@ export default async function handler(
     return;
   }
 
-  if (!question) {
+  if (!question || typeof question !== 'string') {
     return res.status(400).json({ message: 'No question in the request' });
   }
   // OpenAI recommends replacing newlines with spaces for best results
   const sanitizedQuestion = question.trim().replaceAll('\n', ' ');
 
   try {
+    console.log('sanitizedQuestion', sanitizedQuestion, 'formData', formData);
     // const index = pinecone.Index(PINECONE_INDEX_NAME);
 
     // /* create vectorstore*/
@@ -39,15 +59,15 @@ export default async function handler(
     // );
 
     //create chain
-    const chain = await makeChain();
-    //Ask a question using chat history
-    const response = await chain.call({
-      question: sanitizedQuestion,
-      chat_history: history || [],
-    });
+    // const chain = await makeChain();
+    // //Ask a question using chat history
+    // const response = await chain.call({
+    //   question: sanitizedQuestion,
+    //   chat_history: history || [],
+    // });
 
-    console.log('response', response);
-    res.status(200).json(response);
+    // console.log('response', response);
+    // res.status(200).json(response);
   } catch (error: any) {
     console.log('error', error);
     res.status(500).json({ error: error.message || 'Something went wrong' });
