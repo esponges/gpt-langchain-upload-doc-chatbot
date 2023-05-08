26a126cab4ddf7141b0ce75cb86573a1873f729a
esponges <esponges@gmail.com>
2023-05-08 09:40:39 -0600

    try with pdf dist

--- utils/vectorizedFile.ts
+++ utils/vectorizedFile.ts
@@ -1,11 +1,37 @@
 import { OpenAIEmbeddings } from 'langchain/embeddings/openai';
-import { PDFLoader } from 'langchain/document_loaders/fs/pdf';
+// import { PDFLoader } from 'langchain/document_loaders/fs/pdf';
 import { getPineconeIndex } from './pinecone-client';
 import { PineconeStore } from 'langchain/vectorstores/pinecone';
 import { PineconeClient } from '@pinecone-database/pinecone';
-import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter';
+// import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter';
+import * as fs from 'fs';
+import * as pdfjsLib from 'pdfjs-dist/legacy/build/pdf';
+import { Document } from 'langchain/document';
 
 const DOCS_MAX_LENGTH = 150;
+
+// parse pdf into text
+async function extractTextFromPDF(filePath: string): Promise<string> {
+  const data = await fs.promises.readFile(filePath);
+  const loadingTask = pdfjsLib.getDocument({ data });
+
+  const pdf = await loadingTask.promise;
+
+  let text = '';
+  for (let i = 1; i <= pdf.numPages; i++) {
+    const page = await pdf.getPage(i);
+    const content = await page.getTextContent();
+
+    const textItems = content.items.filter((item) =>
+      item.hasOwnProperty('str'),
+    );
+    const pageText = textItems.map((item) => item.str).join('');
+
+    text += pageText;
+  }
+
+  return text;
+}
 
 export const langchainPineconeUpsert = async (
   filePath: string,
@@ -14,12 +40,14 @@ export const langchainPineconeUpsert = async (
 ) => {
   // use pdfjs to load pdf
   // https://js.langchain.com/docs/modules/indexes/document_loaders/examples/file_loaders/pdf
-  const loader = new PDFLoader(filePath, {
-    pdfjs: () => import('pdfjs-dist/legacy/build/pdf.js'),
-    splitPages: false,
-  });
+  // const loader = new PDFLoader(filePath, {
+  //   pdfjs: () => import('pdfjs-dist/legacy/build/pdf.js'),
+  //   splitPages: false,
+  // });
 
-  const pdf = await loader.load();
+  const pdfDistText = await extractTextFromPDF(filePath);
+
+  // const pdf = await loader.load();
   // const content = pdf[0].pageContent;
   // const metadata = pdf[0].metadata;
 
@@ -27,17 +55,25 @@ export const langchainPineconeUpsert = async (
   const pineconeIndex = await getPineconeIndex(pineconeClient);
 
   /* Split text into chunks */
-  const textSplitter = new RecursiveCharacterTextSplitter({
-    chunkSize: 1000,
-    chunkOverlap: 200,
-  });
+  // const textSplitter = new RecursiveCharacterTextSplitter({
+  //   chunkSize: 1000,
+  //   chunkOverlap: 200,
+  // });
 
-  const docs = await textSplitter.splitDocuments(pdf);
-  
+  // const docs = await textSplitter.splitDocuments(pdf);
+  const docs = [
+    new Document({
+      metadata: { test: 'foo' },
+      pageContent: pdfDistText,
+    }),
+  ];
+
   // todo: add threshold for big documents
   if (docs.length > DOCS_MAX_LENGTH) {
     throw new Error('Please upload a smaller document');
   }
+
+  // throw new Error('indexed already');
 
   // add documents to index
   await PineconeStore.fromDocuments(docs, new OpenAIEmbeddings(), {
@@ -46,5 +82,7 @@ export const langchainPineconeUpsert = async (
     namespace: fileName,
   });
 
-  return pdf;
+  // throw new Error('indexed');
+
+  // return pdf;
 };
