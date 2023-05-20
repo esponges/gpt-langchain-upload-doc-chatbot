9ee773fdd94f5ea83844ae8e7e3d1256834b7514
esponges <esponges@gmail.com>
2023-05-20 12:17:44 -0600

    ingest lib for vercel size

--- utils/langchain.ts
+++ utils/langchain.ts
@@ -1,4 +1,5 @@
-import { readFile } from 'fs/promises';
+import * as fs from 'fs';
+import * as pdfjsLib from 'pdfjs-dist/legacy/build/pdf';
 
 import { OpenAIEmbeddings } from 'langchain/embeddings/openai';
 import { PDFLoader } from 'langchain/document_loaders/fs/pdf';
@@ -6,7 +7,6 @@ import { getPineconeIndex } from './pinecone';
 import { PineconeStore } from 'langchain/vectorstores/pinecone';
 import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter';
 import { Document } from 'langchain/document';
-import { BaseDocumentLoader } from 'langchain/document_loaders';
 
 import { PineconeClient } from '@pinecone-database/pinecone';
 
@@ -101,60 +101,35 @@ export const langchainPineconeUpsert = async (
   });
 };
 
-export abstract class BufferLoader extends BaseDocumentLoader {
-  constructor(public filePathOrBlob: string | Blob) {
-    super();
+/*
+ * This is alternative to using pdfjs-dist, pdfjs-dist is the way that is mentioned
+ * by the langchain docs but (it adds the line breaks \n\n\ correctly) but I'm not sure
+ * if it's the best way to do it. I'm leaving this here for now in case I need to use it
+ */
+const extractTextFromPDF = async (filePath: string): Promise<string> => {
+  const data = await fs.promises.readFile(filePath);
+  const loadingTask = pdfjsLib.getDocument({ data });
+
+  const pdf = await loadingTask.promise;
+
+  let text = '';
+  for (let i = 1; i <= pdf.numPages; i++) {
+    const page = await pdf.getPage(i);
+    const content = await page.getTextContent();
+
+    // had to add this since this type since I'd get a type error
+    // despite filtering out the items that don't have the str property
+    type StringItem = (typeof content.items)[0] & { str: string };
+
+    const textItems = content.items.filter((item) =>
+      item.hasOwnProperty('str'),
+    ) as StringItem[];
+
+    const pageText = textItems.map((item) => item.str).join('');
+
+    text += pageText;
   }
 
-  protected abstract parse(
-    raw: Buffer,
-    metadata: Document['metadata'],
-  ): Promise<Document[]>;
+  return text;
+};
 
-  public async load(): Promise<Document[]> {
-    let buffer: Buffer;
-    let metadata: Record<string, string>;
-    if (typeof this.filePathOrBlob === 'string') {
-      buffer = await readFile(this.filePathOrBlob);
-      metadata = { source: this.filePathOrBlob };
-    } else {
-      buffer = await this.filePathOrBlob
-        .arrayBuffer()
-        .then((ab) => Buffer.from(ab));
-      metadata = { source: 'blob', blobType: this.filePathOrBlob.type };
-    }
-    return this.parse(buffer, metadata);
-  }
-}
-
-export class CustomPDFLoader extends BufferLoader {
-  public async parse(
-    raw: Buffer,
-    metadata: Document['metadata'],
-  ): Promise<Document[]> {
-    const { pdf } = await PDFLoaderImports();
-    const parsed = await pdf(raw);
-    return [
-      new Document({
-        pageContent: parsed.text,
-        metadata: {
-          ...metadata,
-          pdf_numpages: parsed.numpages,
-        },
-      }),
-    ];
-  }
-}
-
-async function PDFLoaderImports() {
-  try {
-    // the main entrypoint has some debug code that we don't want to import
-    const { default: pdf } = await import('pdf-parse/lib/pdf-parse.js');
-    return { pdf };
-  } catch (e) {
-    console.error(e);
-    throw new Error(
-      'Failed to load pdf-parse. Please install it with eg. `npm install pdf-parse`.',
-    );
-  }
-}
