7bcfffb721b70b1a11bdb3682fb273a48579f80d
esponges <esponges@gmail.com>
2023-05-23 17:15:53 -0600

    add test route again without unnecessary code

--- pages/api/test-route.ts
+++ pages/api/test-route.ts
@@ -1,25 +1,4 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { Form } from 'multiparty';
-
-import { langchainPineconeUpsert } from '@/utils/langchain';
-import { getPineconeExistingNamespaces, pinecone } from '@/utils/pinecone';
-import { getErrorMessage } from '@/utils/misc';
-
-interface FData {
-  file: {
-    fieldName: string;
-    originalFilename: string;
-    path: string;
-    headers: {
-      [key: string]: string;
-    };
-    size: number;
-  };
-}
-
-interface ApFDataRequest extends NextApiRequest {
-  body: FData;
-}
 
 export type UploadResponse = {
   fileExistsInDB: boolean;
@@ -27,14 +6,9 @@ export type UploadResponse = {
 };
 
 export default async function handler(
-  req: ApFDataRequest,
+  req: NextApiRequest,
   res: NextApiResponse,
 ) {
-  //only accept post requests
-  if (req.method !== 'POST') {
-    res.status(405).json({ error: 'Method not allowed' });
-    return;
-  }
 
   // test route is working in Vercel
   throw new Error('Test route is working in Vercel');
