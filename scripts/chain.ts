12e6d70ec660f5c86c49873d1af994dec2f8a73f
esponges <esponges@gmail.com>
2023-06-18 07:46:44 -0600

    use memory for hnswlib

---
+++ scripts/chain.ts
@@ -0,0 +1,48 @@
+import { OpenAI } from 'langchain/llms/openai';
+import { ConversationalRetrievalQAChain } from 'langchain/chains';
+import { HNSWLib } from 'langchain/vectorstores/hnswlib';
+import { OpenAIEmbeddings } from 'langchain/embeddings/openai';
+import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter';
+import { BufferMemory } from 'langchain/memory';
+import { PDFLoader } from 'langchain/document_loaders/fs/pdf';
+import * as fs from 'fs';
+
+// it useas HNSWLib as a vectorstore and stores the file in memory
+export const runBuiltInMemory = async () => {
+  /* Initialize the LLM to use to answer the question */
+  const model = new OpenAI({});
+  /* Load in the file we want to do question answering over */
+  const loader = new PDFLoader('public/lotr-world-wars.pdf');
+
+  const pdf = await loader.load();
+  /* Split the text into chunks */
+  const textSplitter = new RecursiveCharacterTextSplitter({ chunkSize: 1000 });
+  const docs = await textSplitter.splitDocuments(pdf);
+  console.log('the docs', docs);
+  /* Create the vectorstore */
+  const vectorStore = await HNSWLib.fromDocuments(docs, new OpenAIEmbeddings());
+  /* Create the chain */
+  const chain = ConversationalRetrievalQAChain.fromLLM(
+    model,
+    vectorStore.asRetriever(),
+    {
+      memory: new BufferMemory({
+        memoryKey: 'chat_history', // Must be set to "chat_history"
+      }),
+    },
+  );
+  /* Ask it a question */
+  const question = 'Which were these wars?';
+  const res = await chain.call({ question });
+  console.log(res);
+  /* Ask it a follow up question */
+  const followUpRes = await chain.call({
+    question: 'How many?',
+  });
+  console.log(followUpRes);
+};
+
+(async () => {
+  await runBuiltInMemory();
+  console.log('chain completed');
+})();
--- utils/makechain.ts
+++ utils/makechain.ts
@@ -36,10 +36,12 @@ export const makeChain = async (pineconeClient: PineconeClient, namespace: strin
   );
   
   const model = new OpenAI({
-    temperature: 0.5, // increase temepreature to get more creative answers
+    temperature: 0.9, // increase temepreature to get more creative answers
     modelName: 'gpt-3.5-turbo', //change this to gpt-4 if you have access
     openAIApiKey: process.env.OPENAI_API_KEY,
   });
+
+  const retriever = await vectorStore.asRetriever().getRelevantDocuments('lotr');
 
   return ConversationalRetrievalQAChain.fromLLM(
     model,
