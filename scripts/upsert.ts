ead04ed468da3ddf7dd1af248b72829436dd9edd
esponges <esponges@gmail.com>
2023-06-18 09:13:16 -0600

    upsert and query similarity vectors

--- scripts/upsert.ts
+++ scripts/upsert.ts
@@ -1,14 +1,15 @@
 import { OpenAIEmbeddings } from 'langchain/embeddings/openai';
 import type { UpsertRequest } from '@pinecone-database/pinecone';
 import { getPineconeIndex } from '@/utils/pinecone';
-import { PineconeStore } from 'langchain/vectorstores/pinecone';
-import { OpenAI } from 'langchain';
-import { VectorDBQAChain } from 'langchain/chains';
-import { Document } from 'langchain/document';
-import { Configuration, OpenAIApi } from 'openai';
 import { getErrorMessage } from '@/utils/misc';
 
 const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
+const PINECONE_NAMESPACE = 'european-cities';
+
+// const configuration = new Configuration({
+//   apiKey: OPENAI_API_KEY,
+// });
+// const openai = new OpenAIApi(configuration);
 
 const EUROPEAN_CITIES = [
   'Paris',
@@ -47,6 +48,9 @@ const EUROPEAN_CITIES = [
   'Stuttgart',
 ];
 
+// upsert into a new or existing namespace in pinecone
+// when using one namespace we make sure that the vectors are
+// all together and can be queried together in the following method
 const upsertVectorGroupInPineconeStore = async () => {
   const embeddings = new OpenAIEmbeddings({
     openAIApiKey: OPENAI_API_KEY,
@@ -73,7 +77,7 @@ const upsertVectorGroupInPineconeStore = async () => {
   // create the upsert request
   const upsertRequest: UpsertRequest = {
     vectors,
-    namespace: 'european-cities',
+    namespace: PINECONE_NAMESPACE,
   };
 
   // upsert the vectors in pinecone
@@ -87,7 +91,62 @@ const upsertVectorGroupInPineconeStore = async () => {
   }
 };
 
+// (async () => {
+//   await upsertVectorGroupInPineconeStore();
+//   console.log('upserting vectors in pinecone store');
+// })();
+
+const runPineconeSimilarityQuery = async () => {
+  const embeddings = new OpenAIEmbeddings({
+    openAIApiKey: OPENAI_API_KEY,
+    modelName: 'text-embedding-ada-002',
+  });
+
+  const query = 'Cities where French is spoken';
+  
+  
+  let vectors;
+  try {
+    const langchainEmbed = await embeddings.embedQuery(query);
+
+    /* 
+      using the openai api directly instead of the langchain wrapper
+    */
+    // const embeddings = await openai.createEmbedding({
+    //   model: 'text-embedding-ada-002',
+    //   input: [query],
+    // });
+
+    vectors = langchainEmbed;
+    // console.log('vectors from query embeddings', vectors);
+  } catch (e) {
+    const errMessage = getErrorMessage(e);
+    console.log('error generating query embeddings', errMessage);
+
+    throw new Error(errMessage);
+  }
+
+  const pineconeIndex = await getPineconeIndex();
+
+  try {
+    const pineconeResult = await pineconeIndex.query({
+      queryRequest: {
+        namespace: PINECONE_NAMESPACE,
+        vector: vectors,
+        topK: 5,
+      },
+    });
+
+    console.log('pineconeResult', pineconeResult);
+
+  } catch (e) {
+    const errMessage = getErrorMessage(e);
+    console.log('error querying pinecone', errMessage);
+    throw new Error(errMessage);
+  }
+};
+
 (async () => {
-  await upsertVectorGroupInPineconeStore();
-  console.log('upserting vectors in pinecone store');
+  await runPineconeSimilarityQuery();
+  console.log('running pinecone similarity query');
 })();
