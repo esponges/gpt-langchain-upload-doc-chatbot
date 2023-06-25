45c145d7c5424accb12d187602e0809e5f2b0bb0
esponges <esponges@gmail.com>
2023-06-25 15:55:36 -0600

    drizzle schemas

---
+++ drizzle/schema.ts
@@ -0,0 +1,44 @@
+
+import { relations } from "drizzle-orm";
+import { pgTable, serial, text } from "drizzle-orm/pg-core";
+
+// model LangChainDocs {
+//   id        String   @id @default(uuid())
+//   createdAt DateTime @default(now())
+//   name      String
+//   nameSpace String
+//   docs      Docs[]
+// }
+
+
+// model Docs {
+//   id              String        @id @default(uuid())
+//   createdAt       DateTime      @default(now())
+//   metadata        String // json string
+//   pageContent     String
+//   name            String
+//   docs            LangChainDocs @relation(fields: [langChainDocsId], references: [id])
+//   langChainDocsId String
+// }
+
+export const langChainDocs = pgTable('LangChainDocs', {
+    id: serial('id').primaryKey(),
+    createdAt: text('createdAt'),
+    name: text('name'),
+    nameSpace: text('nameSpace'),
+});
+
+export const langChainDocRelations = relations(langChainDocs, ({ many }) => ({
+    docs: many(docs),
+}));
+
+export const docs = pgTable('Docs', {
+    id: serial('id').primaryKey(),
+    createdAt: text('createdAt'),
+    metadata: text('metadata'),
+    pageContent: text('pageContent'),
+    name: text('name'),
+    langChainDocsId: text('langChainDocsId'),
+});
+
+
